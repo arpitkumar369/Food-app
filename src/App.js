94ffcf8d9fd0bbae6b2c56f410a9a12338f311ae
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2022-12-24 14:02:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import CardsDetails from './components/Cart';
+import Cards from './components/Cards';
+
+
+
+
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Cards />} />
+                <Route path='/cart' element={<CardsDetails />} />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootred from "./redux/reducers/main";
+import thunk from 'redux-thunk';
+
+
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV !== 'production') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose;
+}
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+export const store = createStore(rootred, enhancer);
